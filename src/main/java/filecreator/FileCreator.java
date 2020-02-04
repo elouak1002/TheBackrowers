7e2af97ca4d1f27d01c18c9ca3b126f0cb96115d
Elian Ouaknine <elian-ouaknine@hotmail.fr>
2020-02-04 17:17:40 +0000

    Set the FileCreator structure and main exposed public method

--- src/main/java/filecreator/FileCreator.java
+++ src/main/java/filecreator/FileCreator.java
@@ -1,4 +1,7 @@
-package filecreator;
+package filecreator; 
+
+import java.io.IOException;
+import java.util.Map;
 
 /**
  * Create an output file based on a model one.
@@ -7,7 +10,25 @@ package filecreator;
  * @version 04.02.2020
  */
 public class FileCreator {
-	public FileCreator() {
+
+	private Map<String, TestNode> nodeMap; // TestNode will be replace with Node
+	// private Parser parser;
+
+	/**
+	 * @param nodeMap Map of the file nodes. 
+	 * @param parser Use to parse the input file.
+	 */
+	public FileCreator(Map<String,TestNode> nodeMap /*, Parser parser */ ) { // TestNode will be replaced with Node
+		this.nodeMap = nodeMap;
+		// this.parser = parser;
 	}
 
+	public void processOutputFile() throws IOException {
+		/* TODO HERE */
+		// Get the data lines from the file.
+		// Extract the argument of the data lines as a List.
+		// Modify the X and Y coordinates using the node map.
+		// Recreate the line with the new X and Y coordinates values.
+		// Integrate the modified lines into the whole file.
+	}
 }
