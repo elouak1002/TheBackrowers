b289254f290f141604f74afc7663d3d15fa2d9fe
Elian Ouaknine <elian-ouaknine@hotmail.fr>
2020-02-04 17:04:49 +0000

    Create a FileCreator class structure

---
+++ src/main/java/filecreator/FileCreator.java
@@ -0,0 +1,13 @@
+package filecreator;
+
+/**
+ * Create an output file based on a model one.
+ * Only change the data coordinates for each node,
+ * using the values of the Node Map.
+ * @version 04.02.2020
+ */
+public class FileCreator {
+	public FileCreator() {
+	}
+
+}
