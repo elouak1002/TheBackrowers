a9aa9bf6e165199058a947885aa15031e97e5517
ArchawinWongkittiruk <willypoko@hotmail.com>
2020-02-07 13:03:38 +0000

    Add navigation in hard-code to main

--- src/main/java/GUI/Main.java
+++ src/main/java/GUI/Main.java
@@ -4,13 +4,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main extends Application {
-    Parent inputPage;
-    Parent outputPage;
+    private int currentPage = 0;
 
     public static void main(String[] args) {
         launch(args);
@@ -19,8 +21,9 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         stage.setTitle("TheBackrowers");
-        inputPage = null;
-        outputPage = null;
+
+        Parent inputPage = null;
+        Parent outputPage = null;
         try {
             inputPage = FXMLLoader.load(getClass().getResource("Input.fxml"));
             outputPage = FXMLLoader.load(getClass().getResource("Output.fxml"));
@@ -29,7 +32,35 @@ public class Main extends Application {
         }
         assert inputPage != null;
         assert outputPage != null;
-        Scene scene = new Scene(inputPage);
+
+        ArrayList<Parent> pages = new ArrayList<>();
+        pages.add(inputPage);
+        pages.add(outputPage);
+
+        BorderPane root = new BorderPane();
+        BorderPane navigation = new BorderPane();
+
+        Button previous = new Button("Previous");
+        previous.setOnAction(event -> {
+            if (currentPage > 0) {
+                currentPage--;
+                root.setCenter(pages.get(currentPage));
+            }
+        });
+        Button next = new Button("Next");
+        next.setOnAction(event -> {
+            if (currentPage < pages.size()-1) {
+                currentPage++;
+                root.setCenter(pages.get(currentPage));
+            }
+        });
+
+        navigation.setLeft(previous);
+        navigation.setRight(next);
+        root.setBottom(navigation);
+        root.setCenter(inputPage);
+
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
