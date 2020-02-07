15316b3c188141a4f0f64256ff50c3196de1fee6
aerris <ben.wu_shao_hua@kcl.ac.uk>
2020-02-07 00:18:45 +0000

    Rename Main Class Name for easier merging

--- src/main/java/GUI/InputMain.java
+++ src/main/java/GUI/InputMain.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Main extends Application {
+public class InputMain extends Application {
 
     public static void main(String[] args) {
         launch(args);
