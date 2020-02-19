fb379851ae532b271f2218703d622031f88fbc62
Elian Ouaknine <elian-ouaknine@hotmail.fr>
2020-02-19 12:10:06 +0000

    Change DataLineTest to work with JUnit 5 Paratemerized Testé

--- src/test/java/datastructurestest/DataLineTest.java
+++ src/test/java/datastructurestest/DataLineTest.java
@@ -1,53 +1,39 @@
-//package datastructurestest;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.junit.runners.Parameterized;
-//import org.junit.runners.Parameterized.Parameters;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//import java.util.Arrays;
-//import java.util.Collection;
-//import java.util.ArrayList;
-//
-//import datastructures.DataLine;
-//
-///**
-// * Test the String creation of the Data Line, especially the arguments creation.
-// * @version 12.02.2020
-// */
-//@RunWith(Parameterized.class)
-//public class DataLineTest {
-//
-//	@Parameters
-//	public static Collection<Object[]> data() {
-//		return Arrays.asList(new Object[][] {
-//			{"32.02f", "55.02f", null, "Node name = new Node( 32.02f , 55.02f )"},
-//			{"", "", new ArrayList<String>(Arrays.asList("1st Argument", "2nd Argument", "3rd Argument")), "Node name = new Node( 1st Argument , 2nd Argument , 3rd Argument )"},
-//			{"", "10.0f", new ArrayList<String>(Arrays.asList("1st Argument", "2nd Argument", "3rd Argument")), "Node name = new Node( 10.0f , 1st Argument , 2nd Argument , 3rd Argument )"},
-//			{"10.0f", "10.0f", new ArrayList<String>(Arrays.asList("1st Argument", "2nd Argument", "3rd Argument")), "Node name = new Node( 10.0f , 10.0f , 1st Argument , 2nd Argument , 3rd Argument )"},
-//			{"10.0f", "", new ArrayList<String>(Arrays.asList("1st Argument", "2nd Argument", "3rd Argument")), "Node name = new Node( 10.0f , 1st Argument , 2nd Argument , 3rd Argument )"},
-//			{"10.0f", "", null, "Node name = new Node( 10.0f )"},
-//			{"", "20.0f", null, "Node name = new Node( 20.0f )"}
-//		});
-//	}
-//
-//
-//	private String xCoord;
-//	private String yCoord;
-//	private ArrayList<String> otherArgs;
-//	private String output;
-//	public DataLineTest(String xCoord, String yCoord, ArrayList<String> otherArgs, String output) {
-//		this.xCoord = xCoord;
-//		this.yCoord = yCoord;
-//		this.otherArgs = otherArgs;
-//		this.output = output;
-//	}
-//
-//	@Test
-//	public void stringOutputTest() {
-//		DataLine line = new DataLine("name", xCoord, yCoord, otherArgs, "Node", "Node", "");
-//		assertEquals(line.toString(),output);
-//	}
-//}
+package datastructurestest;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.Arguments;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+import datastructures.DataLine;
+
+/**
+* Test the String creation of the Data Line, especially the arguments creation.
+* @version 12.02.2020
+*/
+public class DataLineTest {
+
+	private static Stream<Arguments> ArgumentsProvider() {
+		return Stream.of(
+			Arguments.of("32.02f", "55.02f", null, "Node name = new Node( 32.02f , 55.02f )"),
+			Arguments.of("", "", new ArrayList<String>(Arrays.asList("1st Argument", "2nd Argument", "3rd Argument")), "Node name = new Node( 1st Argument , 2nd Argument , 3rd Argument )"),
+			Arguments.of("", "10.0f", new ArrayList<String>(Arrays.asList("1st Argument", "2nd Argument", "3rd Argument")), "Node name = new Node( 10.0f , 1st Argument , 2nd Argument , 3rd Argument )"),
+			Arguments.of("10.0f", "10.0f", new ArrayList<String>(Arrays.asList("1st Argument", "2nd Argument", "3rd Argument")), "Node name = new Node( 10.0f , 10.0f , 1st Argument , 2nd Argument , 3rd Argument )"),
+			Arguments.of("10.0f", "", new ArrayList<String>(Arrays.asList("1st Argument", "2nd Argument", "3rd Argument")), "Node name = new Node( 10.0f , 1st Argument , 2nd Argument , 3rd Argument )"),
+			Arguments.of("10.0f", "", null, "Node name = new Node( 10.0f )"),
+			Arguments.of("", "20.0f", null, "Node name = new Node( 20.0f )")
+		);
+	}
+
+	@ParameterizedTest
+	@MethodSource("ArgumentsProvider")
+	public void stringOutputTest(String xCoord, String yCoord, ArrayList<String> otherArgs, String output) {
+		DataLine line = new DataLine("name", xCoord, yCoord, otherArgs, "Node", "Node", "");
+		assertEquals(line.toString(),output);
+	}
+}
