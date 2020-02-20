bac0b61f68f2802c8903cccba6b156f2e6a28aa6
aerris <benwu1997@gmail.com>
2020-02-20 22:29:39 +0000

    drag an drop : in progress [can only drag and drop single input] todo : displaying the files [easy but can't seem to work], drag and drop multiple files[easy, but not as easy as displaying files], select multiple files[difficulty unknown]

--- src/main/java/GUI/MultipleLoadController.java
+++ src/main/java/GUI/MultipleLoadController.java
@@ -2,10 +2,9 @@ package GUI;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.TransferMode;
 import javafx.stage.FileChooser;
-import javafx.stage.Window;
-
-import java.awt.*;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -17,6 +16,8 @@ public class MultipleLoadController {
 
     @FXML private Button uploadButton;
     @FXML private javafx.scene.control.Label selectedFileLabel;
+    @FXML private javafx.scene.control.ListView listView;
+
 
     @FXML
     public void initialize() {}
@@ -27,16 +28,44 @@ public class MultipleLoadController {
 
     @FXML
     void chooseFiles() {
+        //initialise fileChooser
         FileChooser fileChooser = new FileChooser();
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
-        List<File> files = fileChooser.showOpenMultipleDialog(null);
-        for(File printFiles : files){
+        //allow multiple selection of files
+        List<File> selectFiles = fileChooser.showOpenMultipleDialog(null);
+
+        for(File printFiles : selectFiles){
             String showFiles = printFiles.getAbsolutePath();
+            //testing purposes
             System.out.println(showFiles);
+
             fullPath = Paths.get(showFiles);
+            //listView.getItems().addAll(showFiles);
             setLabelText("Selected File: " + showFiles);
         }
     }
+
+    @FXML
+    private void handleDragOver(DragEvent event){
+        if(event.getDragboard().hasFiles()){
+            event.acceptTransferModes(TransferMode.ANY);
+        }
+    }
+
+    @FXML
+    private void handleDrop(DragEvent event){
+        File selectedFile = event.getDragboard().getFiles().get(0);
+        if(selectedFile != null ){
+            if(selectedFile.getName().endsWith(".txt")){
+                fullPath = selectedFile.toPath();
+                setLabelText("Selected File: " + selectedFile.getName());
+                System.out.println(selectedFile.getName());
+            } else {
+                setLabelText("Only .txt files allowed");
+            }
+        }
+    }
+
 
     private void setLabelText(String display){
         selectedFileLabel.setText(display);
