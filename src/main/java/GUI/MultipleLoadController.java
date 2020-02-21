df72dbe1628a3c750d59f33365a3671f9cef22f9
aerris <benwu1997@gmail.com>
2020-02-21 01:43:05 +0000

    managed to display file in listview. IT WAS WORKING ALL ALONG. SCENEBUILDER BUGGED OUT ON MY LAPTOP todo : displaying the files [easy but can't seem to work], select multiple files[difficulty unknown]

--- src/main/java/GUI/MultipleLoadController.java
+++ src/main/java/GUI/MultipleLoadController.java
@@ -1,13 +1,16 @@
 package GUI;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.TransferMode;
 import javafx.stage.FileChooser;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collection;
 import java.util.List;
 
 public class MultipleLoadController {
@@ -16,7 +19,9 @@ public class MultipleLoadController {
 
     @FXML private Button uploadButton;
     @FXML private javafx.scene.control.Label selectedFileLabel;
-    @FXML private javafx.scene.control.ListView listView;
+    //@FXML private javafx.scene.control.ListView listView;
+    @FXML
+    ListView<String> listView = new ListView<String>();
 
 
     @FXML
@@ -40,9 +45,17 @@ public class MultipleLoadController {
             System.out.println(showFiles);
 
             fullPath = Paths.get(showFiles);
-            //listView.getItems().addAll(showFiles);
             setLabelText("Selected File: " + printFiles.getAbsolutePath().substring(printFiles.getAbsolutePath().lastIndexOf("\\") + 1 ));
+
+
         }
+        if (selectFiles != null) {
+            for (int i = 0; i < selectFiles.size(); i++) {
+                listView.getItems().add(selectFiles.get(i).getAbsolutePath());
+            }
+        }
+
+        //listView.getItems().addAll(selectFiles);
     }
 
     @FXML
