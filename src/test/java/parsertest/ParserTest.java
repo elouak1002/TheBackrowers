f848762953c2bdbde1bf9f6d5e178fa537cf3173
Elian Ouaknine <elian-ouaknine@hotmail.fr>
2020-02-21 16:22:58 +0000

    Change order of TestCompile + erase unused imports

--- src/test/java/parsertest/ParserTest.java
+++ src/test/java/parsertest/ParserTest.java
@@ -19,7 +19,6 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.hamcrest.MatcherAssert.assertThat; 
 import static org.hamcrest.Matchers.*;
 
