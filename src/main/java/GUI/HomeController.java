554c8165fdd874f7333bfa49e90a602f0a1b86ac
Harald Gerhardsen <k1889292@kcl.ac.uk>
2020-02-23 18:00:38 +0000

    Refactor HomeController and Home.fxml
    Remove onAction methods for wranglerButton and xmlButton in HomeController; functionality is assigned in Root. Remove Boolean field in HomeController (unused). Remove Label from Home.fxml (unused), and update Home.fxml with corresponding controller changes.

--- src/main/java/GUI/HomeController.java
+++ src/main/java/GUI/HomeController.java
@@ -5,8 +5,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HomeController{
-
-	private Boolean choseWrangler = null;
 	
 	//FXML Components and initialisation
 	@FXML
@@ -16,39 +14,7 @@ public class HomeController{
 	private Button xmlButton;
 
 	@FXML
-	private Label chosenLabel;
-
-	@FXML
 	public void initialize(){}
-
-
-	//Action methods
-
-	/**
-	* If user clicked wranglerButton.
-	*/
-	@FXML
-	private void chooseWrangler(){
-		choseWrangler = new Boolean(true);
-		chosenLabel.setText("Data wrangling");
-	}
-
-	/**
-	* If user clicked xmlButton.
-	*/
-	@FXML
-	private void chooseXML(){
-		choseWrangler = new Boolean(false);
-		chosenLabel.setText("XML generation");
-	}
-
-	/**
-	* Returns what the user has chosen, to do XML generation or data wrangling.
-	* Returns true if user chose wrangling, false if XML.
-	*/
-	public Boolean getChoseWrangler(){
-		return choseWrangler;
-	}
 
 	/**
 	* Returns the XML option button.
