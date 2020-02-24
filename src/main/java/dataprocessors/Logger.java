0725dfe6ee96e00ca22aac223777c37eb002ba85
asfand987 <asfand987.ak@gmail.com>
2020-02-24 23:22:49 +0000

    added Logger class

---
+++ src/main/java/dataprocessors/Logger.java
@@ -0,0 +1,16 @@
+package dataprocessors;
+
+public class Logger {
+
+    public Logger() {
+
+    }
+
+    public void logAdd(String nodeNameA, String nodeNameB) {
+
+    }
+
+    public void lofRemove(String nodeName) {
+
+    }
+}
