c45c7a20549a36ca62f5fa29274aa5b75b4b7551
asfand987 <asfand987.ak@gmail.com>
2020-02-25 13:46:49 +0000

    Code clean up on OutputController

--- src/main/java/dataprocessors/Logger.java
+++ src/main/java/dataprocessors/Logger.java
@@ -24,17 +24,24 @@ public class Logger {
 
     public void saveLoggerToFile() {
         String fileName = "LogFile.txt";
+
         try {
             FileWriter fw = new FileWriter(fileName);
             Writer output = new BufferedWriter(fw);
             int size = logger.size();
+
             for (int i = 0; i < size; i++) {
                 output.write(logger.get(i).toString() + "\n");
             }
+
             output.close();
         }
         catch (IOException e) {
             java.util.logging.Logger.getLogger(OutputController.class.getName()).log(Level.SEVERE, null, e);
         }
+    }
+
+    public void saveFileToDir() {
+
     }
 }
