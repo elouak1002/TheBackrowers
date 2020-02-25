a2b876cc04bdf59554bb4b0de424a34c411bb285
asfand987 <asfand987.ak@gmail.com>
2020-02-25 13:30:18 +0000

    added ability to save logger file

--- src/main/java/dataprocessors/Logger.java
+++ src/main/java/dataprocessors/Logger.java
@@ -1,6 +1,10 @@
 package dataprocessors;
 
+import GUI.OutputController;
+
+import java.io.*;
 import java.util.ArrayList;
+import java.util.logging.Level;
 
 public class Logger {
     private ArrayList<String> logger = new ArrayList<>();
@@ -18,7 +22,19 @@ public class Logger {
         logger.add(removedNode);
     }
 
-    public void outputLogger() {
-
+    public void saveLoggerToFile() {
+        String fileName = "LogFile.txt";
+        try {
+            FileWriter fw = new FileWriter(fileName);
+            Writer output = new BufferedWriter(fw);
+            int size = logger.size();
+            for (int i = 0; i < size; i++) {
+                output.write(logger.get(i).toString() + "\n");
+            }
+            output.close();
+        }
+        catch (IOException e) {
+            java.util.logging.Logger.getLogger(OutputController.class.getName()).log(Level.SEVERE, null, e);
+        }
     }
 }
