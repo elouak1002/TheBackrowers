f0d718eab779ac5cc0ef083519ccf00b39519c45
MihailBratanov <m.d.bratanov@gmail.com>
2020-02-28 15:51:48 +0000

    added comments to the xmlCreator class

--- src/main/java/dataprocessors/XMLCreator.java
+++ src/main/java/dataprocessors/XMLCreator.java
@@ -12,17 +12,31 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
+/**
+ * The xmlCreator class takes a map of Nodes and their order
+ * and creates a new List of string lines which can be written to an xml file.
+ */
 public class XMLCreator{
     private List<String> finalXMLData;
     private TreeMap<String, Node> nodes;
     private ArrayList<String> nodeOrder;
 
+    /**
+     * Constructor of the xmlCreator class - takes a map of Nodes and an arrayList of their order .
+     * @param nodes - map of Node objects mapped to strings
+     * @param nodeOrder - ArrayList of the nodes which gives the input order
+     */
     public XMLCreator(TreeMap<String, Node> nodes, ArrayList<String> nodeOrder){
         this.nodes = nodes;
         this.nodeOrder = nodeOrder;
         finalXMLData = new ArrayList<>();
     }
 
+    /**
+     * This method when called fills the empty finalXMLData list
+     * with the xml string lines.
+     * @return a list of String
+     */
     public List<String> createXMLFile() {
         createHeader();
         createNodeContent();
@@ -30,11 +44,17 @@ public class XMLCreator{
         return finalXMLData;
     }
 
+    /**
+     * Creates the final line of the xml file.
+     */
     private void createFooter() {
         String footer = "</MappinData>";
         this.finalXMLData.add(footer);
     }
 
+    /**
+     * Makes sure each node is parsed into correct xml format.
+     */
     private void createNodeContent() {
         for(String nodename: nodeOrder){
             addXMLentry(nodes.get(nodename));
@@ -42,6 +62,11 @@ public class XMLCreator{
 
     }
 
+    /**
+     * Method that takes as a parameter a node and converts it into an xml line.
+     * Then adds that line to the finalXMLData arrayList.
+     * @param node the node to be transformed
+     */
     private void addXMLentry(Node node) {
         String xmlLine = " <" + node.getType().toLowerCase() + " id=" + "\"" + node.getId() + "\""
         + " x=" + "\""+node.getX() + "\"" + " y=" + "\""+node.getY()+"\""
@@ -63,6 +88,10 @@ public class XMLCreator{
         finalXMLData.add(" </"+node.getType().toLowerCase() +">");
     }
 
+    /**
+     * This method ensures that the file begins with the appropriate
+     * date, time, year, xml version and encoding.
+     */
     private void createHeader() {
         this.finalXMLData.add("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
         DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
