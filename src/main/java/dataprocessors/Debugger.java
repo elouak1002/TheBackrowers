9dc593cfd55666990c51c03345119bd879b61ddf
Elian Ouaknine <elian-ouaknine@hotmail.fr>
2020-03-01 17:28:27 +0000

    Add a neighbour to a node only if it is an initialised node

--- src/main/java/dataprocessors/Debugger.java
+++ src/main/java/dataprocessors/Debugger.java
@@ -4,6 +4,7 @@ import java.util.Iterator;
 import java.util.Map;
 
 import datastructures.Node;
+import datastructures.Status;
 
 /**
  * A debugger, responsible for debugging the output file before it's creation.
@@ -30,34 +31,39 @@ public class Debugger {
 	private void addAsNeighbour(Node nodeA, Node nodeB) {
 		if(!nodeA.hasNeighbour(nodeB)) {
 			nodeA.addNeighbour(nodeB);
+			System.out.println("Add " + nodeB.getName() + " to " + nodeA.getName());
 			// Logger.logAdd(nodeA.getName(),nodeB.getName()) // Log the addition of nodeB as a neighbor of nodeA.
 		}
 	}
-
-
+	
+	
 	/**
 	 * Run over the key of the map,
 	 * and add neighbours to each node if needed.
+	 * Add neighbours only to initialised nodes.
 	 */
 	private void addExistingNeighbours() {
 		for (String nodeName : nodeMap.keySet()) {
 			Node node = nodeMap.get(nodeName);
 			for (Node neighbour : node.getNeighbours()) {
-				addAsNeighbour(neighbour, node);
+				if (neighbour.getStatus() == Status.INITIALISED) {
+					addAsNeighbour(neighbour, node);
+				}
 			}
 		}
 	}
-
+	
 	/**
 	 * Remove the Nodes without neighbours from the Map.
 	 */
 	private void removeNeighbourlessNodes() {
 		Iterator<Map.Entry<String,Node>> iter = nodeMap.entrySet().iterator();
 		while (iter.hasNext()) {
-    		Map.Entry<String,Node> node = iter.next();
+			Map.Entry<String,Node> node = iter.next();
     		if(node.getValue().getNeighbours().isEmpty()){
 				// Logger.logRemove(nodeName); // Log the deletion of a Node from the file to the logfile. 1st possibility.
 				// notifyRemove(nodeName); // Basically the same, a form of Observer design Pattern.
+				System.out.println("remove " + node.getValue().getName());
         		iter.remove();
     		}
 		}
