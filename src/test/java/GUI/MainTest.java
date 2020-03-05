9f34375651dccfa795cfff82fd6e458f3b12d2d9
garg-shubh <shubhgarg2000@gmail.com>
2020-03-05 23:02:24 +0000

    start GUI testing

---
+++ src/test/java/GUI/MainTest.java
@@ -0,0 +1,23 @@
+package GUI;
+
+import javafx.stage.Stage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest extends ApplicationTest {
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+
+}
