76b0170502ef1b13d05c8aa177d830dcc19d698c
garg-shubh <shubhgarg2000@gmail.com>
2020-03-06 00:02:51 +0000

    update GUITest

--- src/test/java/GUI/MainTest.java
+++ src/test/java/GUI/MainTest.java
@@ -1,15 +1,35 @@
 package GUI;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.testfx.framework.junit.ApplicationTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobotInterface;
+import org.testfx.framework.junit.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest extends ApplicationTest {
 
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent mainNode = FXMLLoader.load(Main.class.getResource("Home.fxml"));
+        stage.setScene(new Scene(mainNode));
+        stage.show();
+        stage.toFront();
+    }
+
+    @Test
+    void startApplication() throws InterruptedException {
+        wait(1000);
+        FxRobotInterface fxRobotInterface = clickOn("#wranglerButton");
+        wait(1000);
+    }
 
     @BeforeEach
     void setUp() {
