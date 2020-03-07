e0ff02e11f8f3e16665962e8c88b13aef47f499d
asfand987 <asfand987.ak@gmail.com>
2020-03-07 13:19:57 +0000

    Added comments to LoggerController.java and LoggerControllerTest.java

--- src/main/java/GUI/LoggerController.java
+++ src/main/java/GUI/LoggerController.java
@@ -14,6 +14,11 @@ import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * The LoggerController class displays the log window of the GUI where
+ * the user can preview the log file that has been created and make
+ * any changes to it before saving.
+ */
 public class LoggerController {
     @FXML private Button saveButton;
     @FXML private TextArea displayLog;
@@ -21,22 +26,40 @@ public class LoggerController {
     @FXML
     public void initialize() {}
 
+    /**
+     * A method for outputting the contents of the @log ArrayList
+     * in the Debugger class.
+     * @param debugger : Gets the current debugger to access it's @Log.
+     * @param filename : The current name of the file to be displayed
+     *                 at the top of the @displayLog area.
+     */
     void setOutputText(Debugger debugger, String filename) {
         if (!debugger.getLog().isEmpty()) {
             displayLog.appendText("For file '" + filename + "', the following debugging has happened:\n");
+
             for (String string : debugger.getLog()) {
                 displayLog.appendText(string);
             }
+
             displayLog.appendText("\n");
         }
     }
 
+    /**
+     * A method for notifying the user if any changes have been
+     * made to the Log file.
+     * @param debugger : Gets the current debugger to access it's @Log.
+     */
     void setNotification(Debugger debugger) {
         if(!debugger.getLog().isEmpty()) {
             new Alert(Alert.AlertType.INFORMATION, "Log file has been updated.", ButtonType.CLOSE).showAndWait();
         }
     }
 
+    /**
+     * A method for saving the log page into a .txt file
+     * in a directory the user wants.
+     */
     @FXML
     private void saveFileToDir() {
         Window stage = saveButton.getScene().getWindow();
@@ -53,6 +76,12 @@ public class LoggerController {
         }
     }
 
+    /**
+     * A method for writing the @displayLog field into a file
+     * which allows the text to be saved as a .txt file in the saveFileToDir()
+     * method.
+     * @param file file to be saved.
+     */
     private void saveLoggerToFile(File file) {
         try {
             PrintWriter writer;
@@ -64,11 +93,16 @@ public class LoggerController {
         }
     }
 
+    /**
+     * A method for clearing the @displayLog area where the text preview
+     * is displayed.
+     */
     @FXML
     private void clearTextField() {
         if (!displayLog.getText().equals("")) {
             Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "This will clear the log. Are you sure?",
                     ButtonType.YES, ButtonType.CANCEL);
+
             Optional<ButtonType> result = alert.showAndWait();
             if (result.isPresent() && result.get() == ButtonType.YES) {
                 displayLog.clear();
--- src/test/java/guitest/LoggerControllerTest.java
+++ src/test/java/guitest/LoggerControllerTest.java
@@ -7,24 +7,28 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * Test whether the changing of the wrangled file via the debugger is correctly
+ * shown in the log.
+ */
 public class LoggerControllerTest {
     private HashMap<String, Node> nodeMap;
     private List<Node> nodeList;
 
     @BeforeEach
     public void setUp() {
-        nodeList = new ArrayList<Node>();
+        nodeList = new ArrayList<>();
         nodeList.add(new Node("Node1", 10f, 10f));
         nodeList.add(new Node("Node2", 10f, 10f));
         nodeList.add(new Node("Node3", 10f, 10f));
 
-        nodeMap = new HashMap<String, Node>();
+        nodeMap = new HashMap<>();
+
         for (Node node : nodeList) {
             nodeMap.put(node.getName(), node);
         }
@@ -33,22 +37,23 @@ public class LoggerControllerTest {
     @Test
     public void testLogAddNode() {
         nodeMap.get("Node1").addNeighbour(nodeList.get(1));
+
         Debugger debugger = new Debugger(nodeMap);
 
-        String loggerOutput = "Node1 added to become a neighbour for Node2.";
-        final String cleanOutput = debugger.getLog().get(0).replaceAll("\n", "");
+        String expectedOutput = "Node1 added to become a neighbour for Node2.";
+        final String loggerOutput = debugger.getLog().get(0).replaceAll("\n", "");
 
-        assertEquals(loggerOutput, cleanOutput);
+        assertEquals(expectedOutput, loggerOutput);
     }
 
     @Test
     public void testLogRemoveNode() {
         Debugger debugger = new Debugger(nodeMap);
         
-        String loggerOutput = "Node1 has no neighbour, so it was removed.";
-        final String cleanOutput = debugger.getLog().get(0).replaceAll("\n", "");
+        String expectedOutput = "Node1 has no neighbour, so it was removed.";
+        final String loggerOutput = debugger.getLog().get(0).replaceAll("\n", "");
 
-        assertEquals(loggerOutput, cleanOutput);
+        assertEquals(expectedOutput, loggerOutput);
     }
 
     @Test
@@ -60,6 +65,7 @@ public class LoggerControllerTest {
 
         ArrayList<String> output = new ArrayList<>();
         ArrayList<String> logger;
+
         output.add("Node1 added to become a neighbour for Node2.");
         output.add("Node2 added to become a neighbour for Node3.");
 
@@ -70,9 +76,9 @@ public class LoggerControllerTest {
         for (int i = 0; i < output.size(); i++) {
 
             String loggerOutput = logger.get(i).replaceAll("\n", "");
-            String testOutput = output.get(i).replaceAll("\n", "");
+            String expectedOutput = output.get(i).replaceAll("\n", "");
 
-            assertEquals(testOutput , loggerOutput);
+            assertEquals(expectedOutput , loggerOutput);
         }
     }
 
@@ -82,6 +88,7 @@ public class LoggerControllerTest {
 
         ArrayList<String> output = new ArrayList<>();
         ArrayList<String> logger;
+
         output.add("Node1 has no neighbour, so it was removed.");
         output.add("Node3 has no neighbour, so it was removed.");
         output.add("Node2 has no neighbour, so it was removed.");
@@ -93,9 +100,9 @@ public class LoggerControllerTest {
         for (int i = 0; i < output.size(); i++) {
 
             String loggerOutput = logger.get(i).replaceAll("\n", "");
-            String testOutput = output.get(i).replaceAll("\n", "");
+            String expectedOutput = output.get(i).replaceAll("\n", "");
 
-            assertEquals(testOutput , loggerOutput);
+            assertEquals(expectedOutput , loggerOutput);
         }
     }
 }
