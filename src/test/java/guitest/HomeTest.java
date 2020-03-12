83993752ef93984bc0fdbf16e10bfa8d3243b4a3
ArchawinWongkittiruk <willypoko@hotmail.com>
2020-03-12 17:32:17 +0000

    Add tests from Home page

---
+++ src/test/java/guitest/HomeTest.java
@@ -0,0 +1,42 @@
+package guitest;
+
+import javafx.scene.control.Labeled;
+import org.junit.jupiter.api.Test;
+
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.matcher.control.LabeledMatchers.hasText;
+
+public class HomeTest extends AppRunner {
+    @Test
+    public void wranglerButtonWorks() {
+        Labeled wranglerButton = lookup("#wranglerButton").query();
+        verifyThat(wranglerButton, hasText("Data Wrangler"));
+        clickOn(wranglerButton);
+        if (lookup("#uploadButton").tryQuery().isPresent()) {//this waits for the button to become visible
+            Labeled uploadButton = lookup("#uploadButton").query();
+            verifyThat(uploadButton, hasText("Upload File"));
+        }
+    }
+
+    @Test
+    public void xmlButtonWorks() {
+        Labeled xmlButton = lookup("#xmlButton").query();
+        verifyThat(xmlButton, hasText("XML Generator"));
+        clickOn(xmlButton);
+        if (lookup("#saveButton").tryQuery().isPresent()) {//this waits for the button to become visible
+            Labeled saveButton = lookup("#saveButton").query();
+            verifyThat(saveButton, hasText("Merge and Save"));
+        }
+    }
+
+    @Test
+    public void logButtonWorks() {
+        Labeled logButton = lookup("#log").query();
+        verifyThat(logButton, hasText("Log"));
+        clickOn(logButton);
+        if (lookup("#saveButton").tryQuery().isPresent()) {//this waits for the button to become visible
+            Labeled saveButton = lookup("#saveButton").query();
+            verifyThat(saveButton, hasText("Save"));
+        }
+    }
+}
