8d493ea0c6142ed51f184b43e7b851e939ee4b2b
Elian Ouaknine <elian-ouaknine@hotmail.fr>
2020-03-13 21:41:54 +0000

    Add a class to test the XMLDebugger

---
+++ src/test/java/dataprocessorstest/XMLDebuggerTest.java
@@ -0,0 +1,78 @@
+package dataprocessorstest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import dataprocessors.XMLDebugger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.hamcrest.collection.IsMapContaining;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+
+import datastructures.Node;
+import datastructures.Status;
+
+/** 
+ * Test the Added method to the XML Debugger (removing uninitialised neighbours) 
+ * @version 12.02.2020
+*/
+public class XMLDebuggerTest {
+
+	private HashMap<String, Node> nodeMap;
+	private List<Node> nodeList;
+
+	@BeforeEach
+	private void setUp() {
+		nodeList = new ArrayList<Node>();
+		nodeList.add(new Node("Node1", 10f, 10f));
+		nodeList.add(new Node("Node2", 10f, 10f));
+
+		nodeMap = new HashMap<String, Node>();
+		for (Node node : nodeList) {
+			nodeMap.put(node.getName(), node);
+		}
+	}
+
+	@Test
+	public void testUnitialisedNeighboursAreKept() {
+		HashMap<String, Node> expectedOutcome = new HashMap<>();
+        Node node1 = new Node("Node1",10f,10f);
+		Node node2 = new Node("Node2",10f,10f);
+
+		node1.setNeighbours(Arrays.asList(node1));
+        node2.setNeighbours(Arrays.asList(node2));
+
+        expectedOutcome.put("Node1",node1);
+		expectedOutcome.put("Node2",node2);
+
+		// Add neighbour for node 1
+		nodeMap.get("Node1").setNeighbours(Arrays.asList(new Node("NodeA",Status.ONLY_NEIGHBOUR),new Node("NodeB",Status.ONLY_NEIGHBOUR)));
+		nodeMap.get("Node2").setNeighbours(Arrays.asList(new Node("NodeA",Status.ONLY_NEIGHBOUR),new Node("NodeB",Status.ONLY_NEIGHBOUR)));
+		
+		// Add neighbour for node 1
+		nodeMap.get("Node1").addNeighbour(nodeList.get(1));
+		
+		// Add neighbour for node 2
+		nodeMap.get("Node2").addNeighbour(nodeList.get(0));
+		
+		XMLDebugger XMLdebugger = new XMLDebugger(nodeMap);
+
+		HashMap<String, Node> actualOutcome = new HashMap<>(XMLdebugger.getMap());
+
+		Comparator<Node> byName = (Node nodeA, Node nodeB) -> node1.getName().compareTo(node2.getName());
+
+		for (String nodeName : actualOutcome.keySet()) {
+			actualOutcome.get(nodeName).getNeighbours().sort(byName);
+			expectedOutcome.get(nodeName).getNeighbours().sort(byName);
+            assertEquals(actualOutcome.get(nodeName).getNeighbours().toString(), expectedOutcome.get(nodeName).getNeighbours().toString());
+		}
+	}
+}
