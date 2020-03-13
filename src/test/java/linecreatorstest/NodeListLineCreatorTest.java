ce34e531decd7e4b3e821334f3de0a7e231b534d
Elian Ouaknine <elian-ouaknine@hotmail.fr>
2020-03-13 20:59:02 +0000

    Test for the NodeListLineCreator

--- src/test/java/linecreatorstest/NodeListLineCreatorTest.java
+++ src/test/java/linecreatorstest/NodeListLineCreatorTest.java
@@ -9,9 +9,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.stream.Stream;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.io.IOException;
-import java.nio.file.Paths;
 
 import linecreators.LineCreator;
 import linecreators.NodeListLineCreator;
@@ -26,40 +25,52 @@ public class NodeListLineCreatorTest {
 	@ParameterizedTest
 	private static Stream<Arguments> ArgumentsProvider() {
 		return Stream.of(
-			Arguments.of(new HashMap<String, Node>() {{
-				put("HenRaph_04_493_264", new Node("HenRaph_04_493_264", 1.03f, 2.32f,"Node"));
-				put("HenRaph_04_476_264", new Node("HenRaph_04_476_264", 10.34f, 20.54f, "Node"));
-				put("HenRaph_04_374_347", new Node("HenRaph_04_374_347", 14.03f, 24.54f,"Node"));
-				put("HenRaph_04_418_357", new Node("HenRaph_04_418_357", 7.01f, 27.03f,"Node"));
-				put("HenRaph_04_419_365", new Node("HenRaph_04_419_365", 32.13f, 27.21f,"Node"));
+			Arguments.of(new LinkedHashMap<String, Node>() {{
+				put("Node1", new Node("Node1", 1.03f, 2.32f,"Node"));
+				put("Node2", new Node("Node2", 10.34f, 20.54f, "Node"));
+				put("Node3", new Node("Node3", 14.03f, 24.54f,"Node"));
+				put("Node4", new Node("Node4", 7.01f, 27.03f,"Node"));
+				put("Node5", new Node("Node5", 32.13f, 27.21f,"Node"));
 			}},
 			new ArrayList<String>(Arrays.asList(
 				"====== NODE LISTS =======",
 				"",
-				"",
-				"",
-				"",
-				""
+				"// Nodes:",
+				" , Node1 , Node2 , Node3 , Node4 , Node5"
 				))),
-			Arguments.of(new HashMap<String, Node>() {{
-				put("HenRaph_04_493_264", new Node("HenRaph_04_493_264", 1.03f, 2.32f,"Node"));
-				put("HenRaph_04_476_264", new Node("HenRaph_04_476_264", 10.34f, 20.54f,"Room"));
-				put("HenRaph_04_374_347", new Node("HenRaph_04_374_347", 14.03f, 24.54f,"Toilet"));
+			Arguments.of(new LinkedHashMap<String, Node>() {{
+				put("Node1", new Node("Node1", 1.03f, 2.32f,"Node"));
+				put("Node2", new Node("Node2", 10.34f, 20.54f,"Room"));
+				put("Node3", new Node("Node3", 14.03f, 24.54f,"Toilet"));
+				put("Node4", new Node("Node4", 14.03f, 24.54f,"FloorChanger"));
 			}},
 			new ArrayList<String>(Arrays.asList(
+				"====== NODE LISTS =======",
+				"",
+				"// Nodes:",
+				" , Node1",
+				"",
+				"// Rooms:",
+				" , Node2",
+				"",
+				"// Toilets:",
+				" , Node3",
+				"",
+				"// FloorChangers:",
+				" , Node4"
 				))),
 			Arguments.of(null,
 			new ArrayList<String>(Arrays.asList(
 				))),
-			Arguments.of(new HashMap<String, Node>() {{}},new ArrayList<String>(Arrays.asList()))
+			Arguments.of(new LinkedHashMap<String, Node>() {{}},new ArrayList<String>(Arrays.asList()))
 		);
 
 	}
 
 	@ParameterizedTest
 	@MethodSource("ArgumentsProvider")
-	public void getModifiedLinesTest(HashMap<String,Node> nodeMap, ArrayList<String> output) throws IOException {
-		LineCreator nodeListLineCreator = new NodeListLineCreator(nodeMap,Paths.get("src/test/resources/testData.txt"));
+	public void getModifiedLinesTest(LinkedHashMap<String,Node> nodeMap, ArrayList<String> output) throws IOException {
+		LineCreator nodeListLineCreator = new NodeListLineCreator(nodeMap,null);
 
 		assertEquals(nodeListLineCreator.getLines(),output);
 	}
