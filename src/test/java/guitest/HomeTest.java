9c58efa12b3ab09e92a38db9dd24fc5271a1e49a
garg-shubh <shubhgarg2000@gmail.com>
2020-03-15 22:05:41 +0530

    add guiLoaded test to HomeTest

--- src/test/java/guitest/HomeTest.java
+++ src/test/java/guitest/HomeTest.java
@@ -7,10 +7,22 @@ import static org.testfx.api.FxAssert.verifyThat;
 import static org.testfx.matcher.control.LabeledMatchers.hasText;
 
 public class HomeTest extends AppRunner {
+
+    @Test
+    public void guiLoaded(){
+        Labeled wranglerButton = lookup("#wranglerButton").query();
+        verifyThat(wranglerButton, hasText("Data Wrangler"));
+
+        Labeled xmlButton = lookup("#xmlButton").query();
+        verifyThat(xmlButton, hasText("XML Generator"));
+
+        Labeled logButton = lookup("#log").query();
+        verifyThat(logButton, hasText("Log"));
+    }
+
     @Test
     public void wranglerButtonWorks() {
         Labeled wranglerButton = lookup("#wranglerButton").query();
-        verifyThat(wranglerButton, hasText("Data Wrangler"));
         clickOn(wranglerButton);
         if (lookup("#uploadButton").tryQuery().isPresent()) {//this waits for the button to become visible
             Labeled uploadButton = lookup("#uploadButton").query();
@@ -21,7 +33,6 @@ public class HomeTest extends AppRunner {
     @Test
     public void xmlButtonWorks() {
         Labeled xmlButton = lookup("#xmlButton").query();
-        verifyThat(xmlButton, hasText("XML Generator"));
         clickOn(xmlButton);
         if (lookup("#saveButton").tryQuery().isPresent()) {//this waits for the button to become visible
             Labeled saveButton = lookup("#saveButton").query();
@@ -32,7 +43,6 @@ public class HomeTest extends AppRunner {
     @Test
     public void logButtonWorks() {
         Labeled logButton = lookup("#log").query();
-        verifyThat(logButton, hasText("Log"));
         clickOn(logButton);
         if (lookup("#saveButton").tryQuery().isPresent()) {//this waits for the button to become visible
             Labeled saveButton = lookup("#saveButton").query();
