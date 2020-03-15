8a7b8deccb95f79410c2a45cd44f847cce8e0edf
garg-shubh <shubhgarg2000@gmail.com>
2020-03-16 01:41:12 +0530

    create LoadTest and add guiLoaded test to it

---
+++ src/test/java/guitest/LoadTest.java
@@ -0,0 +1,37 @@
+package guitest;
+
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.Labeled;
+import javafx.scene.control.TextArea;
+import org.junit.jupiter.api.Test;
+import org.testfx.matcher.control.TextInputControlMatchers;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.matcher.control.LabeledMatchers.hasText;
+public class LoadTest extends AppRunner{
+
+    @Test
+    public void guiLoaded(){
+        Labeled wranglerButton = lookup("#wranglerButton").query();
+        verifyThat(wranglerButton, hasText("Data Wrangler"));
+        clickOn(wranglerButton);
+        if (lookup("#uploadButton").tryQuery().isPresent()) {//this waits for the button to become visible
+            Labeled uploadButton = lookup("#uploadButton").query();
+            verifyThat(uploadButton, hasText("Upload File"));
+        }
+        if (lookup("#dragTarget").tryQuery().isPresent()) {//this waits for the text area to become visible
+            TextArea dragTarget = lookup("#dragTarget").query();
+            assertTrue(dragTarget.isVisible());
+            assertTrue(dragTarget.getPromptText().equals("Drop file here..."));
+        }
+        if (lookup("#selectedFileLabel").tryQuery().isPresent()) {//this waits for the label to become visible
+            Label selectedFileLabel = lookup("#selectedFileLabel").query();
+            assertTrue(selectedFileLabel.isVisible());
+            assertTrue(selectedFileLabel.getText().equals("Selected File: "));
+        }
+
+    }
+
+}
