6e32e101547ae6583cedccce75b9b8af52b761db
Elian Ouaknine <elian-ouaknine@hotmail.fr>
2020-03-21 16:40:42 +0100

    Remove unused imports

--- src/test/java/guitest/LoadTest.java
+++ src/test/java/guitest/LoadTest.java
@@ -1,11 +1,9 @@
 package guitest;
 
-import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.Labeled;
 import javafx.scene.control.TextArea;
 import org.junit.jupiter.api.Test;
-import org.testfx.matcher.control.TextInputControlMatchers;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.testfx.api.FxAssert.verifyThat;
