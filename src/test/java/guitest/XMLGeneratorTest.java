bb2ac68ba5d96f56478a6fe39dcfe8128752ad6b
garg-shubh <shubhgarg2000@gmail.com>
2020-03-25 14:45:40 +0530

    Add tests for XMLGenerator

--- src/test/java/guitest/XMLGeneratorTest.java
+++ src/test/java/guitest/XMLGeneratorTest.java
@@ -2,6 +2,8 @@ package guitest;
 import GUI.Root;
 import javafx.scene.control.Label;
 import javafx.scene.control.Labeled;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testfx.api.FxToolkit;
@@ -29,6 +31,25 @@ public class XMLGeneratorTest extends AppRunner {
         clickButton("#uploadButton");
         uploadFile(findPath("XMLParserTestData2.txt"));
 
+        ListView uploadTable = lookup("#uploadTable").query();
+
+        uploadTable.getFocusModel().focus(0);
+        uploadTable.getSelectionModel().select(0);
+        clickButton("#selectedFiles");
+
+        uploadTable.getFocusModel().focus(0);
+        uploadTable.getSelectionModel().select(0);
+        clickButton("#selectedFiles");
+
+        clickButton("#next");
+        clickOn("Close");
+
+        TextArea outputText = lookup("#outputText").query();
+        String output = outputText.getText();
+
+        assertTrue(output.contains(" <room id=\"3\" x=\"47.61459\" y=\"26.463207\" Floor=\"4\" name=\"HR 4.2\">\n" +
+                "  <neighbour id=\"8\"/>\n" +
+                " </room>"));
     }
 
 }
